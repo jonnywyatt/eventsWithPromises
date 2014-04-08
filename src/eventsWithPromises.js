8fd20ebed5a0adc812f0f079421085ea5480f46a
Jon Wyatt <jon.wyatt@moneyadviceservice.co.uk>
2014-04-08 21:43:53 +0100

    unsubscribe, unsubscribeAll methods added. Publish method correctly resolves master promise correctly

--- src/eventsWithPromises.js
+++ src/eventsWithPromises.js
@@ -2,6 +2,7 @@ define(['jquery'], function ($) {
   var subscriptions = {};
 
   return {
+
     subscribe: function (eventName, callback, context) {
       var found = false;
 
@@ -30,36 +31,55 @@ define(['jquery'], function ($) {
       }
     },
 
+    unsubscribe: function(eventName, context) {
+      if (!eventName || (typeof eventName !== 'string')) {
+        throw "Event name not supplied to unsubscribe method";
+      }
+      if (!context || (typeof context !== 'object')) {
+        throw "Context not supplied to unsubscribe method";
+      }
+      if (subscriptions[eventName]) {
+        for (var i = 0, j = subscriptions[eventName].length; i < j; i++) {
+          if (subscriptions[eventName][i].context === context) {
+            subscriptions[eventName].splice(i, 1);
+            break;
+          }
+        }
+      }
+    },
+
+    unsubscribeAll: function() {
+      subscriptions = {};
+    },
+
     publish: function (eventName, data, promise) {
       var subscriptionsForEvent,
           promises = [];
 
       if (subscriptions[eventName]) {
         subscriptionsForEvent = subscriptions[eventName];
-        data = data || {};
-        for (var i = 0, j = subscriptionsForEvent.length; i < j; i++) {
-          if (promise) {
-            data.promise = $.Deferred();
-            promises.push(data.promise);
-          }
-          if (subscriptionsForEvent[i].context) {
-            subscriptionsForEvent[i].context.call(context, data)
-          } else {
-            subscriptionsForEvent[i].callback(data);
-          }
-        }
         if (promise) {
-          debugger;
-          $.when(promises)
+          for (var i = 0, j = subscriptionsForEvent.length; i < j; i++) {
+            promises.push($.Deferred());
+          }
+          $.when.apply($, promises)
               .done(function () {
                 promise.resolve();
               })
               .fail(function () {
                 promise.reject();
-              })
+              });
+        }
+        for (var i = 0, j = subscriptionsForEvent.length; i < j; i++) {
+          if (subscriptionsForEvent[i].context) {
+            subscriptionsForEvent[i].callback.call(subscriptionsForEvent[i].context, data, promises[i] || null)
+          } else {
+            subscriptionsForEvent[i].callback(data, promises[i] || null);
+          }
         }
 
       }
     }
+
   }
 });
--- test/eventsWithPromises_spec.js
+++ test/eventsWithPromises_spec.js
@@ -1,6 +1,6 @@
 describe("subscription", function () {
 
-  before(function (done) {
+  beforeEach(function (done) {
     var self = this;
     requirejs(['src/eventsWithPromises', 'jquery'], function (eventsWithPromises, $) {
       self.eventsWithPromises = eventsWithPromises;
@@ -10,6 +10,11 @@ describe("subscription", function () {
 
   });
 
+  afterEach(function() {
+    this.eventsWithPromises.unsubscribeAll();
+    this.eventsWithPromises = null;
+  });
+
   it("adds a subscription if event name and callback supplied", function () {
     var spy = sinon.spy(),
         data = {
@@ -17,22 +22,81 @@ describe("subscription", function () {
         };
     this.eventsWithPromises.subscribe('myEventType', spy);
     this.eventsWithPromises.publish('myEventType', data);
-    assert(spy.calledWith(data));
+    sinon.assert.calledWith(spy, data);
   });
 
-  it("calls a callback on a context if supplied", function() {
+  xit("throws an error if subscription params not supplied", function() {
 
   });
 
-  it("sends a promise to each listener if event is published with a promise", function () {
-    var spy1 = sinon.spy(),
-        spy2 = sinon.spy();
-    this.eventsWithPromises.subscribe('myEvent', function(data) {
-      assert(data.promise !== null, 'Promise supplied to listener')
+  it("removes a subscription if event name and context supplied", function () {
+    var spy = sinon.spy(),
+        myObj = {};
+    this.eventsWithPromises.subscribe('myEventType', spy, myObj);
+    this.eventsWithPromises.publish('myEventType');
+    this.eventsWithPromises.unsubscribe('myEventType', myObj);
+    this.eventsWithPromises.publish('myEventType');
+    sinon.assert.calledOnce(spy);
+  });
+
+  it("executes the callback with a context if supplied", function () {
+    var spy = sinon.spy(),
+        myObj = {};
+    this.eventsWithPromises.subscribe('myEventType2', spy, myObj);
+    this.eventsWithPromises.publish('myEventType2');
+    sinon.assert.calledOn(spy, myObj);
+  });
+
+  it("sends a promise to each listener if event is published with a promise", function (done) {
+    this.eventsWithPromises.subscribe('myEvent', function (data, promise) {
+      assert(promise !== null, 'Promise supplied to listener');
+      done();
     });
-    this.eventsWithPromises.subscribe('myEvent', spy2);
+    this.eventsWithPromises.publish('myEvent', null, this.$.Deferred());
+  });
 
-    this.eventsWithPromises.publish('myEventType', null, this.$.Deferred());
+  describe("resolution of promises", function () {
+
+    beforeEach(function (done) {
+      this.promise = this.$.Deferred();
+      this.eventsWithPromises.subscribe('myEvent', function (data, promise) {
+        promise.resolve();
+      });
+      this.eventsWithPromises.subscribe('myEvent', function (data, promise) {
+        promise.resolve();
+      });
+      this.promise.always(function() {
+        done();
+      });
+      this.eventsWithPromises.publish('myEvent', null, this.promise);
+    });
+
+    it("resolves the promise sent with the event when all listeners have resolved theirs", function () {
+      assert.equal(this.promise.state(), 'resolved', 'promise resolved');
+    });
+
+  });
+
+  describe("rejection of promises", function () {
+
+    beforeEach(function (done) {
+      this.promise = this.$.Deferred();
+      this.eventsWithPromises.subscribe('myEvent2', function (data, promise) {
+        promise.reject();
+      });
+      this.eventsWithPromises.subscribe('myEvent2', function (data, promise) {
+        promise.resolve();
+      });
+      this.promise.always(function() {
+        done();
+      });
+      this.eventsWithPromises.publish('myEvent2', null, this.promise);
+    });
+
+    it("rejects the promise sent with the event when at least one listener has rejected theirs", function () {
+      assert.equal(this.promise.state(), 'rejected', 'promise resolved');
+    });
+
   });
 
 });
